5ca8e0162abfc8bb3b72d2796029ccf4b73e0b58
fhdkdbb706 <52700377+fhdkdbb706@users.noreply.github.com>
2019-07-11 15:15:08 +0900

    start

---
+++ HelloWorld_and_comment.java
@@ -0,0 +1,18 @@
+public class HelloWorld_and_comment {
+    // start
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+//      System.out.println("test");
+/*      System.out.println("1");
+        System.out.println("2"); */
+        System.out.println("3");
+    }
+}
+
+// line comment -> ctrl + /
+/* block comment */
+
+/** doc comment
+ *
+ */
