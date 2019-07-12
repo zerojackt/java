41b92713e26c0d5e99e6f45295f1e957dfb7f4d7
fhdkdbb706 <52700377+fhdkdbb706@users.noreply.github.com>
2019-07-12 14:18:20 +0900

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,23 @@
+public class variable {
+    public static void main(String[] args) {
+
+        int count;
+
+        count = 10;
+        count = 20;
+        System.out.println(count);
+
+        double avg = 11.1; /* 실수 */
+
+        int totalCount;
+
+//        첫번째 문자가 소문자인 명사로 해야함
+//        두번째 문자부터는 두번쨰 문자 첫글자를 대문자로 한다.
+//        ex) catdog -> catDog
+//        '_' 를 쓰지 않는다.
+        int number =3;
+        System.out.println(number);
+
+
+    }
+}
